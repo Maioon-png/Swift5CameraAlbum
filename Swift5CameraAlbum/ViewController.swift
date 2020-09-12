f11d01b006aa0835dd8189053aa2d7d6cd1c557d
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-12 19:37:44 +0900

    first commite

--- Swift5CameraAlbum/ViewController.swift
+++ Swift5CameraAlbum/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+//カメラを使ってもいいかユーザーに確認
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var backImageView: UIImageView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            switch(status){
+                
+                
+            case .notDetermined:
+                print("notDetermined")
+            case .restricted:
+                print("restricted")
+            case .denied:
+                print("許可されていません。")
+            case .authorized:
+                print("許可されています。")
+            }
+        }
+        
     }
-
-
+    //カメラボタン
+    @IBAction func openCamera(_ sender: Any) {
+        let sourceType = UIImagePickerController.SourceType.camera
+        //カメラが利用可能かチェック
+        
+        
+    }
+    
 }
 
