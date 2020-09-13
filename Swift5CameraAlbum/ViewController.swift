7e396c4094af13b0afd37ab1646a0d9611284e9e
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-13 16:35:40 +0900

    add buttons

--- Swift5CameraAlbum/ViewController.swift
+++ Swift5CameraAlbum/ViewController.swift
@@ -38,9 +38,69 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     @IBAction func openCamera(_ sender: Any) {
         let sourceType = UIImagePickerController.SourceType.camera
         //カメラが利用可能かチェック
-        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            //変数化
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            //編集を許す
+            cameraPicker.allowsEditing = true
+            present(cameraPicker, animated: true, completion: nil)
+            
+        }else{
+            print("エラー")
+        }
         
     }
     
+    //カメラキャンセル
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Pickerが閉じる
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    //アルバムボタン
+    @IBAction func openAlbum(_ sender: Any) {
+        let sourceType = UIImagePickerController.SourceType.photoLibrary
+        //カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            //変数化
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            //編集を許す
+            cameraPicker.allowsEditing = true
+            present(cameraPicker, animated: true, completion: nil)
+            
+        }else{
+            print("エラー")
+        }
+    }
+    
+    //撮った写真、アルバムから選択した写真のでーたを受け取る
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //infoの中に画像が入っていたら
+        if let pickedImage = info[.editedImage] as? UIImage{
+            //UIImageViewに画像を差し込む
+            backImageView.image = pickedImage
+            //写真の保存
+            UIImageWriteToSavedPhotosAlbum(pickedImage, self, nil, nil)
+        }
+        //画面を閉じる
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    //シェアするための機能
+    @IBAction func share(_ sender: Any) {
+        let text = "#test"
+        let image = backImageView.image?.jpegData(compressionQuality: 0.2)
+        let items = [text, image] as [Any]
+        let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(activityVC, animated: true, completion: nil)
+    }
+    
 }
+
 
